78d4dfe65aeb6f4f2c03bee8b9f175a240463802
nimmakayala-durgasushma <59272351+nimmakayala-durgasushma@users.noreply.github.com>
2020-01-14 03:03:50 -0800

    Add files via upload

---
+++ OneWebA.java
@@ -0,0 +1,46 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class OneWebA {
+
+	public static void main(String[] args) 
+	{
+		 Properties objprop = new Properties();
+		 try
+		 {
+		    objprop.load(new FileInputStream("C:/Users/admin/documents//data1.properties"));
+		 }
+		 catch(FileNotFoundException e)
+		 {
+			 e.printStackTrace();
+		 }
+		 catch(IOException e)
+		 {
+			 e.printStackTrace();
+		 }
+		 System.out.println("login name"+objprop.getProperty("login"));
+		 System.out.println("password"+objprop.getProperty("pwd"));
+		 System.setProperty("webdriver.chrome.driver", "c:/Users/admin/downloads//chromedriver_win32//chromedriver.exe"); 
+		 WebDriver ObjDriver = new ChromeDriver();
+	     ObjDriver.get("http://demo.opencart.com");
+	     System.out.println("Open cart page loaded");
+		 String var1 = ObjDriver.getTitle();
+		 System.out.println("Title of the Page"+var1);
+		 WebElement Obj1 = ObjDriver.findElement(By.xpath("//a[@href='https://demo.opencart.com/index.php?route=account/account']"));
+		 Obj1.click();
+		 WebElement Obj2 = ObjDriver.findElement(By.xpath("//*[@id='top-links']/ul/li[2]/ul/li[2]/a"));
+		 Obj2.click();
+		 WebElement Obj3 = ObjDriver.findElement(By.xpath("//*[@id='input-email']"));
+		 Obj3.sendKeys(objprop.getProperty("login"));
+		 WebElement Obj4 = ObjDriver.findElement(By.xpath("//*[@id='input-password']"));
+		 Obj4.sendKeys(objprop.getProperty("pwd"));
+
+	}
+
+}
---
+++ Reistration1.java
@@ -0,0 +1,33 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Reistration1 {
+
+	public static void main(String[] args) 
+	{
+		System.setProperty("webdriver.chrome.driver",
+	   			"C://Users//admin//Downloads//chromedriver_win32/chromedriver.exe");
+		 WebDriver ObjDriver=new ChromeDriver();
+		 ObjDriver.get("http://demo.automationtesting.in/SignIn.html");
+		 //demo.automationtesting.in/Register.html
+
+		 ObjDriver.findElement(By.xpath("//*[@id='enterbtn']")).click();
+		WebElement Obj= ObjDriver.findElement(By.xpath("//*[@id='errormsg']"));
+		System.out.println(Obj.getText());
+		 
+		 try
+		 {
+			 Thread.sleep(5000);
+			 
+		 }catch(InterruptedException e){
+			 e.printStackTrace();
+		 }
+	        
+	     ObjDriver.quit();
+		 
+
+	}
+
+}
